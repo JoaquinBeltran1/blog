164e26f6b1097d692b0b15365e52f0f467ab0199
Joaquin Beltran <j.beltranbaldy@gmail.com>
2022-03-20 22:16:16 +0100

    minor

--- gh-pages.js
+++ gh-pages.js
@@ -6,7 +6,7 @@ publish(
   branch: 'gh-pages',
   repo: 'https://github.com/JoaquinBeltran1/joaquinbeltran1.github.io.git', // Update to point to your repository
   user: {
-   name: 'Joaquin Beltra', // update to use your name
+   name: 'Joaquin Beltran', // update to use your name
    email: 'j.beltranbaldy@gmail.com' // Update to use your email
   },
   dotfiles: true
