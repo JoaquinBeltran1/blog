4349f84facd76322134344e32d1c727c2c9fdc2c
Joaquin Beltran <j.beltranbaldy@gmail.com>
2022-03-21 21:07:55 +0100

    smol changes

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,29 @@
-<header>
-  <a href="/">Home</a>
+<script>
+  import { page } from "$app/stores";
+</script>
 
-  <nav>
-    <ul>
-      <li>
-        <a href="/blog">Blog</a>
-      </li>
-      <li>
-        <a href="/about">About</a>
-      </li>
-    </ul>
-  </nav>
+<header>
+  <ul class="flex justify-center space-x-4 pt-4 text-xl font-semibold blue">
+    <li class="hover:text-blue-400" class:active={$page.url.pathname === "/"}>
+      <a href="/">Home</a>
+    </li>
+    <li class="hover:text-blue-400" class:active={$page.url.pathname === "/blog"}>
+      <a href="/blog">Blog</a>
+    </li>
+    <li class="hover:text-blue-400" class:active={$page.url.pathname === "/about"}>
+      <a href="/about">About</a>
+    </li>
+  </ul>
 </header>
+
+<style>
+  .active {
+    text-decoration: underline;
+  }
+  .active:hover {
+    color: rgb(30 64 175);
+  }
+  .blue {
+    color: rgb(52, 78, 162);
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,10 +3,22 @@
   import "../app.css";
 </script>
 
-<Header />
+<div class="justify-center px-4 test m-auto bg-red-200">
+  <Header />
 
-<main>
-  <slot />
-</main>
+  <main>
+    <slot />
+  </main>
 
-<footer>Hello, I'm the footer.</footer>
+  <footer>Hello, I'm the footer.</footer>
+</div>
+
+<style>
+  @media (min-width: 1150px) {
+    .test {
+      width: 1150px;
+      padding-left: 6px;
+      padding-right: 6px;
+    }
+  }
+</style>
