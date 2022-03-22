29b322bd621e474bc0ffb32476123472cff072cf
Joaquin Beltran <j.beltranbaldy@gmail.com>
2022-03-22 12:56:36 +0100

    gh pages should work

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <div class="blue md:pt-32">
   <div class="text-5xl font-extralight leading-normal">
-    <h1>Wescosme to my personal website.</h1>
+    <h1>Welcome to my personal website.</h1>
     <h1>Where I write about Data & Business Analysis.</h1>
   </div>
 </div>
